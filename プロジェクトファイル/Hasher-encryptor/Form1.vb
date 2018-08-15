f9a0de7ce31c10cbb56d6e521eff3b4fa508c8a0
kyoe29 <oefamilymail@gmail.com>
2018-08-15 14:55:26 +0900

    make buttonenable()

--- プロジェクトファイル/Hasher-encryptor/Form1.vb
+++ プロジェクトファイル/Hasher-encryptor/Form1.vb
@@ -12,9 +12,22 @@
     Private Sub TextBox1_DragDrop(sender As Object, e As DragEventArgs) Handles TextBox1.DragDrop
         executePath = e.Data.GetData(DataFormats.FileDrop)(0)
         TextBox1.Text = executePath
+        buttonenable(True)
     End Sub
 
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
         TextBox1.Text = executePath
+    End Sub
+    Public Sub buttonenable(ByVal change As Boolean)
+        Select Case change
+            Case True
+                GroupBox2.Enabled = True
+            Case False
+                GroupBox2.Enabled = False
+        End Select
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        buttonenable(False)
     End Sub
 End Class
